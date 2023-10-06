ce55e31261058aedc6ca022a013685deef9c80e9
Apples4 <nhlanhla.mavuka@gmail.com>
2023-10-06 09:43:02 +0200

    edited q2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     if len(argv) - 1 == 0:
         print("0 arguments.")
     elif len(argv) - 1 == 1:
-        print("1 argument")
+        print("1 argument:")
     else:
-        print("{0} arguments".format(len(argv) - 1))
+        print("{0} arguments:".format(len(argv) - 1))
     for x in range(1, len(argv)):
         print("{0}: {1}".format(x, str(argv[x])))
