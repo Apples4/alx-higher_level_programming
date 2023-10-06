1ce935b79018ec39e64a57f87fd937d97da20759
Apples4 <nhlanhla.mavuka@gmail.com>
2023-10-06 09:40:46 +0200

    edited q1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     if len(argv) - 1 == 0:
-        print("{0} arguments.")
+        print("0 arguments.")
     elif len(argv) - 1 == 1:
         print("1 argument")
     else:
