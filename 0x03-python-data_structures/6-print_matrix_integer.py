5fe1187395be11048de0b09cca84c223a984161d
Apples4 <nhlanhla.mavuka@gmail.com>
2023-10-09 14:02:15 +0200

    edited question 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,4 +5,4 @@ def print_matrix_integer(matrix=[[]]):
             print("{0:d}".format(matrix[i][j]), end="")
             if j != (len(matrix[i]) - 1):
                 print(" ", end="")
-        print()
+        print("")
