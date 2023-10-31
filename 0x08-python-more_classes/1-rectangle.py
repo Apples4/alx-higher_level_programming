1a8374cd96fb0f12f3db22124e11876c8e6541cd
Apples4 <nhlanhla.mavuka@gmail.com>
2023-10-31 10:00:15 +0200

    edited docstring task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,9 +5,11 @@ Class that defines a rectangle
 
 
 class Rectangle:
-
+    '''
+    function for class
+    '''
     def __init__(self, width=0, height=0):
-
+        ''' init function '''
         self.__height = 0
         self.__width = 0
         self.width = width
@@ -15,12 +17,12 @@ class Rectangle:
 
     @property
     def height(self):
-
+    ''' getter for height '''
         return self.__height
 
     @height.setter
     def height(self, value):
-
+    ''' setter for height '''
         if type(value) != int:
             raise TypeError("height must be an integer")
 
@@ -30,12 +32,12 @@ class Rectangle:
 
     @property
     def width(self):
-
+        ''' getter for width '''
         return self.__width
 
     @width.setter
     def width(self, value):
-
+    ''' setter for width '''
         if type(value) != int:
             raise TypeError("width must be an integer")
 
