6ec5e4a0b481832a9153e442c8fa4d04f37f9ada
Apples4 <nhlanhla.mavuka@gmail.com>
2023-10-31 09:45:13 +0200

    pycodestyle error for task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
 
-        if type(value) != int: 
+        if type(value) != int:
             raise TypeError("height must be an integer")
 
         if value < 0:
@@ -34,7 +34,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
 
-        if type(value) != int: 
+        if type(value) != int:
             raise TypeError("width must be an integer")
 
         if value < 0:
