75c7c28cffa14b48a3599c3e806bd5d5a701676a
Apples4 <nhlanhla.mavuka@gmail.com>
2023-11-06 21:41:58 +0200

    changed Task 7 again

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -29,8 +29,9 @@ class BaseGeometry:
             TypeError: <name> must be an integer
             ValueError: <name> must be greater than 0
         '''
-
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+        self.name = name
+        self.value = value
+        if type(self.value) != int:
+            raise TypeError("{} must be an integer".format(self.name))
+        if self.value <= 0:
+            raise ValueError("{} must be greater than 0".format(self.name))
