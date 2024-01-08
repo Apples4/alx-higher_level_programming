9aa49860917644c09f5ef484cf12df4cab3975f8
Apples4 <nhlanhla.mavuka@gmail.com>
2024-01-09 01:24:58 +0200

    ecit task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -5,7 +5,6 @@ const { argv } = require('node:process');
 if (argv[2] === undefined) {
   console.log('No argument');
 } else {
-  for (let i = 2; i < argv.length; i++) {
-    console.log(argv[i]);
+  console.log(argv[2]);
   }
 }
