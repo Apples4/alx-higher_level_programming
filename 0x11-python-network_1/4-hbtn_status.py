34ef1ee38a8ed0d5638cfe80a9ec750d5bf4f5c4
Apples4 <nhlanhla.mavuka@gmail.com>
2024-01-27 02:27:17 +0200

    eddted task5

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,9 +5,10 @@ import requests
 script that fetches
 https://alx-intranet.hbtn.io/status
 """
+if __name__ == "__main__":
 
-print("Body response:")
-print("\t- type: {}".format(type(requests.get(
-      "https://alx-intranet.hbtn.io/status").text)))
-print("\t- content: {}".format(requests.get(
-      "https://alx-intranet.hbtn.io/status").text))
+    print("Body response:")
+    print("\t- type: {}".format(type(requests.get(
+          "https://alx-intranet.hbtn.io/status").text)))
+    print("\t- content: {}".format(requests.get(
+          "https://alx-intranet.hbtn.io/status").text))
