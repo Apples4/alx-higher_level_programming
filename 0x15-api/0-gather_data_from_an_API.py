77bace2acd37091366462fb4d21c20014903e0d0
Apples4 <nhlanhla.mavuka@gmail.com>
2024-02-21 01:04:07 +0200

    updated files to repo

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,26 @@
 import requests
 import sys
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     user_id = int(sys.argv[1])
 
     # Fetch tasks for userid
-    respon = requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}/todos')
-    todos = respon.json()
+    response = requests.get(f'https://jsonplaceholder.typicode.com'
+                            f'/users/{user_id}/todos')
+
+    todos = response.json()
 
     total_todos = len(todos)
     done_todos = sum(1 for task in todos if task['completed'])
 
     # Fetch username from userid
-    res = requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}')
-    user_name = res.json()['name']  # Corrected line
+    response = requests.get(f'https://jsonplaceholder.typicode.com'
+                            f'/users/{user_id}')
+    user_name = response.json()['name']  # Corrected line
 
     # Show progress
-    print(f'Employee {user_name} is done with tasks({done_todos}/{total_todos}):')
+    print(f'Employee {user_name} is done with'
+          f'tasks({done_todos}/{total_todos}):')
     for task in todos:
         if task['completed']:
             print(f"\t{task['title']}")
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,22 +7,23 @@ if __name__ == '__main__':
     user_id = int(sys.argv[1])
 
     """ Fetch tasks for userid """
-    respon = requests.get(
-                        f'https://jsonplaceholder.typicode.com/users/{user_id}/todos')
-    todos = respon.json()
+    response = requests.get(f'https://jsonplaceholder.typicode.com'
+                            f'/users/{user_id}/todos')
+    todos = response.json()
 
     total_todos = len(todos)
     done_todos = sum(1 for task in todos if task['completed'])
 
     """ Fetch username from userid """
-    res = requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}')
-    user_name = res.json()['name']
+    response = requests.get(f'https://jsonplaceholder.typicode.com'
+                            f'/users/{user_id}')
+    user_name = response.json()['name']
 
     """ creating a csv file """
     header_list = ["USER_ID",
-                    "USERNAME",
-                    "TASK_COMPLETED_STATUS",
-                    "TASK_TITLE"]
+                   "USERNAME",
+                   "TASK_COMPLETED_STATUS",
+                   "TASK_TITLE"]
 
     csv.register_dialect("myDialect",
                          delimiter=",",
